c5085e06d470287dd01b30ef308f067aa2040217
Joseph Reilly <josephreilly@Josephs-MacBook-Air.local>
2022-12-28 16:02:15 -0700

    Title Revision

--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <head>
-    <title>13 template</title>
+    <title>Next with Prisma</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </head>
