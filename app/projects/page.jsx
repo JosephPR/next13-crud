89c246442b7b57f75608097e7b1bf9e5218bf044
Joseph Reilly <josephreilly@Josephs-MacBook-Air.local>
2022-12-29 23:24:52 -0700

    updates

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import { PrismaClient } from '@prisma/client'
+import style from './db.module.css'
+import { formatDate } from '../../lib/utils.js'
+
+
 
 
 export default async function Projects() {
 
-
 const prisma = new PrismaClient()
 const allProjects = await prisma.projects.findMany()
+const allTasks = await prisma.tasks.findMany()
 console.log(allProjects) 
+console.log(allTasks) 
   return (
     <>
-   
-    <h2>Database Projects</h2>
+    <div className={style.db}>
+    <div>
+   <h2>Database Projects</h2>
     <br />
     {allProjects.map((project) => (
       <h3 key={project.id}>{project.name}</h3>
     ))}
+    
+   </div>
+  <div>  
+      <h2>Tasks</h2>
+    <br />
+    {allTasks.map((task) => (
+      <h3 key={task.id}>{task.name} - {formatDate(task.completedDateTime)}</h3>
+    ))}
+    </div>
+    </div>
+
+
     </>
   )
 }
